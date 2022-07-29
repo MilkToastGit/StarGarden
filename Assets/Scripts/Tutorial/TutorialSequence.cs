9c1d98898a34605af742810e6a72f6b53f839e04
Aubrey <aubreyvigus@gmail.com>
2022-07-29 16:59:37 +0800

    Tutorials Set Up

--- Assets/Scripts/Tutorial/TutorialSequence.cs
+++ Assets/Scripts/Tutorial/TutorialSequence.cs
@@ -3,51 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TutorialSequence : MonoBehaviour
+namespace StarGarden.UI
 {
-    public bool Seen => seen;
-    public Button ActivateButton;
-
-    private GameObject baseObject;
-    private GameObject[] slides;
-    private int currentSlide;
-    private bool seen = false;
-
-    private void Awake()
+    public class TutorialSequence : MonoBehaviour
     {
-        ActivateButton?.onClick.AddListener(Show);
+        public static bool TutorialActive = false;
 
-        baseObject = transform.GetChild(0).gameObject;
+        public bool Seen => seen;
+        public UIPanel ActivatePanel;
 
-        slides = new GameObject[baseObject.transform.childCount];
-        for (int i = 0; i < baseObject.transform.childCount; i++)
-            slides[i] = baseObject.transform.GetChild(i).gameObject;
-    }
+        private GameObject baseObject;
+        private GameObject[] slides;
+        private int currentSlide;
+        private bool seen = false;
 
-    public void NextSlide()
-    {
-        if (!baseObject.activeSelf) return;
-
-        if (currentSlide + 1 > slides.Length)
+        private void Awake()
         {
-            Hide();
-            return;
+            ActivatePanel.OnShow += Show;
+
+            baseObject = transform.GetChild(0).gameObject;
+
+            slides = new GameObject[baseObject.transform.childCount - 1];
+            for (int i = 0; i < baseObject.transform.childCount - 1; i++)
+            {
+                slides[i] = baseObject.transform.GetChild(i).gameObject;
+                print(slides[i].name);
+            }
         }
 
-        slides[currentSlide].SetActive(false);
-        slides[++currentSlide].SetActive(true);
-    }
+        public void NextSlide(Vector2 pos = default)
+        {
+            if (!baseObject.activeSelf) return;
+            print($"{currentSlide + 1}, {slides.Length}");
 
-    public void Show()
-    {
-        currentSlide = 0;
-        baseObject.SetActive(true);
-    }
+            if (currentSlide + 1 >= slides.Length)
+            {
+                Hide();
+                return;
+            }
 
-    public void Hide()
-    {
-        seen = true;
-        ActivateButton.onClick.RemoveListener(Show);
-        baseObject.SetActive(false);
+            slides[currentSlide].SetActive(false);
+            slides[++currentSlide].SetActive(true);
+        }
+
+        public void Show()
+        {
+            print("Showing");
+            currentSlide = 0;
+            baseObject.SetActive(true);
+            slides[currentSlide].SetActive(true);
+            Core.InputManager.Main.OnTapCompleted += NextSlide;
+            TutorialActive = true;
+        }
+
+        public void Hide()
+        {
+            print("Hiding");
+            seen = true;
+            ActivatePanel.OnShow -= Show;
+            baseObject.SetActive(false);
+            slides[currentSlide].SetActive(false);
+            Core.InputManager.Main.OnTapCompleted -= NextSlide;
+            TutorialActive = false;
+        }
     }
 }
